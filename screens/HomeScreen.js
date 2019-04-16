27998e358994675c12120fe2c58b1992276146a6
Kylie Chesner <kyliec362@gmail.com>
2019-04-16 00:24:36 -0500

    modal + basic form fields + close modal button

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component,  TouchableHighlight  } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   ListView,
   ScrollView,
   Text,
+  Modal,
   View
 } from 'react-native';
 
+import { Input, Button } from 'react-native-elements';
+
+
 import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-material-cards';
-import { Button } from 'react-native-material-ui';
 import { FloatingAction } from 'react-native-floating-action';
+import Form from 'react-native-form'
+
 
 export default class HomeScreen extends Component {
   static navigationOptions = {
@@ -18,13 +23,18 @@ export default class HomeScreen extends Component {
   };
 
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      isVisible: false, //state of modal default false
+
+    };
   }
+
 
   render() {
     data = {'Plant 1' : {'image' : '../src/static/plant1.jpg', 'strain': 'sativa', 'age' : 22},
-            'Plant 2' : {'image' : '../src/static/plant2.jpg', 'strain': 'sativa', 'age' : 52},
-            'Plant 3' : {'image' : '../src/static/plant3.jpg', 'strain': 'sativa', 'age' : 77}};
+    'Plant 2' : {'image' : '../src/static/plant2.jpg', 'strain': 'sativa', 'age' : 52},
+    'Plant 3' : {'image' : '../src/static/plant3.jpg', 'strain': 'sativa', 'age' : 77}};
 
     cards = [];
     for (var key in data) {
@@ -32,15 +42,15 @@ export default class HomeScreen extends Component {
         image = data[key]['image'];
         cards.push(
           <Card>
-            <CardImage
-              source={require('../src/static/plant1.jpg')}
-              title={"Day " + data[key]['age']}
-            />
-            <CardTitle 
-              title={key} 
-              subtitle={data[key]['strain']}
-            />
-            {/* <CardContent text="Your device will reboot in few seconds once successful, be patient meanwhile" /> */}
+          <CardImage
+          source={require('../src/static/plant1.jpg')}
+          title={"Day " + data[key]['age']}
+          />
+          <CardTitle
+          title={key}
+          subtitle={data[key]['strain']}
+          />
+          {/* <CardContent text="Your device will reboot in few seconds once successful, be patient meanwhile" /> */}
           </Card>
         )
       }
@@ -48,17 +58,104 @@ export default class HomeScreen extends Component {
 
     return (
       <View>
-        <ScrollView>
-          {cards}
-        </ScrollView>
-        <FloatingAction
-          onPressItem={
-            (name) => {
-              console.log(`selected button: ${name}`);
-            }
-          }
-        />
+      <ScrollView>
+      {cards}
+      </ScrollView>
+
+      <View style={styles.modal}>
+
+      <Modal animationType = {"fade"} transparent = {false}
+      visible = {this.state.isVisible}
+      onRequestClose = {() =>{ console.log("Modal has been closed.") } }>
+
+      <View style={styles.modalForm}>
+        <Text style={styles.headerText}>New Plant Form</Text>
+        <Input label='Strain' />
+        <Input label='Description' />
+        <Input label='Age' />
       </View>
+
+
+      <Button
+        onPress={() => this.setState({ isVisible: false})}
+        title="Submit"
+        buttonStyle={styles.submitButton}>
+      </Button>
+
+
+
+
+      </Modal>
+      </View>
+
+      <FloatingAction
+      showBackground = {true}
+      overlayColor = 'rgba(0, 0, 0, 0)'
+      onPressMain = {() => {this.setState({ isVisible: true})}}
+      />
+      </View>
+
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  submitButton:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 'auto',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    marginTop:30
+  },
+  modal: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 5,
+    width: '90%',
+    height: '30%',
+    margin: 5
+  },
+  text: {
+    color: '#3f2949',
+    marginTop: 10
+  },
+  ModalInsideView:{
+
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor : "#00BCD4",
+    height: 300 ,
+    width: '90%',
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#fff'
+
+  },
+  modalForm:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 5,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  }
+});
